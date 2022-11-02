6f2b31c9e58ab1f87285dac60458faddd93ca7cd
emilianhaugland <81188152+EmilianHaugland@users.noreply.github.com>
2022-11-02 14:43:35 +0100

    init

--- Virtual Server/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\emili\source\repos\Test Django\Virtual Server\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
